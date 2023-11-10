3c975d82229079e831bed3541772280ab7380b68
soufyanesraoui24 <soufyanesraoui24@gmail.com>
2023-11-10 20:27:35 +0100

    add files on repository

---
+++ labs3/notation.js
@@ -0,0 +1,14 @@
+
+
+function mean(score) {
+
+    let average =0;
+    for (let i = 0; i < score.length; i++) {
+         average +=  score[i];
+        
+    }
+    console.log(average / (score.length));
+    
+}
+
+module.exports= mean
